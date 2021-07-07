fd58c200f840041dc8aae1fa6ec6f40397fb45fc
Евгений Роев <roev64@mail.ru>
2021-07-07 09:42:07 +0500

    20210707-01

--- _mvn.bat
+++ _mvn.bat
@@ -1,6 +1,13 @@
-set PATH=%PATH%;c:\Disc_C\_Setup\Java\apache-maven-3.8.1\bin
 set M2_HOME=c:\Disc_C\_Setup\Java\apache-maven-3.8.1
+set PATH=%PATH%;c:\Disc_C\_Setup\Java\apache-maven-3.8.1\bin
+
+set JAVA_HOME=c:\Disc_C\_Setup\Java\jdk-15.0.2
+set PATH=%PATH%;c:\Disc_C\_Setup\Java\jdk-15.0.2\bin
 
 
-mvn clean
+rem mvn -version
+rem mvn clean
 mvn package
+
+pause
+
