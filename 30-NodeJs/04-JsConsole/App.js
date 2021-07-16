72128529b27f348659f3ef0b4f8e2bc10485e9a7
Евгений Роев <roev64@mail.ru>
2021-07-16 17:45:33 +0500

    20210715-01

--- 30-NodeJs/04-JsConsole/App.js
+++ 30-NodeJs/04-JsConsole/App.js
@@ -1,69 +1,102 @@
-
 Ext.onReady(function(){
-Ext.define('User', {
-           extend: 'Ext.data.Model',
-                     
-            idProperty: 'userID',
-                     
-            fields: [{
-                name: 'userID',
-                type: 'int'
-            }, {
-                name: 'name',
-                type: 'string'
-            }, {
-                name: 'surname',
-                type: 'string'
-            }, {
-                name: 'date',
-                type: 'date'
-            }, {
-                name: 'email',
-                type: 'string'
-            }, {
-                name: 'married',
-                type: 'bool'
-            }],
-      });
 
-var store = Ext.create('Ext.data.Store', {
-                    model: 'User',
-                    autoLoad: true,
-                    proxy: {
-                            type: 'ajax',
-                            url: 'users.json',
-                            reader: {
-                                type: 'json',
-                                root: 'users'
-                            }
-                }
-        });
 
-Ext.create('Ext.grid.Panel', {
-        title: 'Пользователи',
-        height: 200,
+
+var yourData = [
+     [1, 'JavaScript'],
+    [2, 'PHP'],
+    [3, 'RUBY']
+];
+
+    Ext.create('Ext.Panel', {
+        width:500,
+        height:120,
+        items: [{
+            xtype: 'combobox',
+            fieldLabel: 'Выберите язык',
+            store: new Ext.data.SimpleStore({
+                 id:0,
+                fields:
+                [
+                    'myId',   //числовое значение - номер элемента
+                    'myText' //текст
+                ],
+                data:yourData
+            }),
+            valueField:'myId',
+            displayField:'myText',
+            queryMode:'local'
+        }],
+        renderTo: Ext.getBody()
+    });
+
+    var store2 = Ext.create('Ext.data.Store', {
+        fields: ['name'],
+        data:[
+             {name: 'JavaScript'},
+            {name: 'PHP'},
+            {name: 'RUBY'},
+            {name: 'Python'},
+            {name: 'C (ANSI)'},
+            {name: 'C++'},
+            {name: 'C#'}
+        ]
+    });
+    Ext.create('Ext.form.ComboBox', {
+        fieldLabel: 'Выбрать язык',
+        store: store2,
+        queryMode: 'local',
+        displayField: 'name',
+        valueField: 'name',
+        typeAhead: true,
+        typeAheadDelay: 100,
+        hideTrigger: true,
+        renderTo: Ext.getBody()
+    });
+
+
+    var store3 = Ext.create('Ext.data.Store', {
+        fields: ['position', 'title', 'rate'],
+        data:[{
+                    position: 1,
+                    title: 'C',
+                    rate: '19.224'
+                }, {
+                    position: 2,
+                    title: 'Java',
+                    rate: '17.455'
+                }, {
+                    position: 3,
+                    title: 'Objective-C',
+                    rate: '10.383'
+                }, {
+                    position: 4,
+                    title: 'C++',
+                    rate: '9.689'
+                }, {
+                    position: 5,
+                    title: 'PHP',
+                    rate: '5.732'
+                }]
+    });
+    Ext.create('Ext.Panel', {
+        title: 'Рейтинг TIOBE',
         width: 400,
-        store: store,
-        columns: [{
-            header: 'Имя',
-            dataIndex: 'name'
-        }, {
-            header: 'Фамилия',
-            dataIndex: 'surname'
-        }, {
-            header: 'Дата рождения',
-            dataIndex: 'date',
-            xtype:'datecolumn',
-            format: 'd/m/Y',
-            flex:1
-        }, {
-            header: 'E-mail',
-            dataIndex: 'email',
-            flex:1
-        }, {
-            header: 'Женат/Замужем',
-            dataIndex: 'married',
-            flex:1
+        autoHeight: true,
+        bodyPadding: 10,
+        items: [{
+            xtype: 'combobox',
+            fieldLabel: 'Выбрать язык',
+            displayField: 'title',
+            store: store3,
+            queryMode: 'local',
+            anchor: '100%',
+            listConfig: {
+                getInnerTpl: function(){
+                return '<h3>{title} ({position})</h3>' +
+                '<div class="rate">Текущее значение: {rate}</div>';
+                }
+            }
         }],
         renderTo: Ext.getBody()
     });
