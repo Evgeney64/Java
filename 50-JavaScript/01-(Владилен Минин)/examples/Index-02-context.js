c2d932687de54cf04d9ba932ebda8b01855d31f1
Евгений Роев <roev64@mail.ru>
2021-07-28 10:53:04 +0500

    20210726-01

--- 50-JavaScript/01-(Владилен Минин)/examples/Index-02-context.js
+++ 50-JavaScript/01-(Владилен Минин)/examples/Index-02-context.js
@@ -1,12 +1,64 @@
+function hello(){
+    console.log("Привет !!! ", this)
+}
 const person = {
-    name: 'Maxim',
+    name: 'Владилен',
     age: 25,
-    greet: function(){
-        console.log('Привет !!!')
+    sayHello: hello,
+
+    // привязка контекста
+    sayHelloWindow: hello.bind(window),
+    sayHelloThis: hello.bind(this),
+    sayHelloDocument: hello.bind(document),
+    // ........................
+
+    logInfo: function(job, phone){
+        // обратные кавычки <~,Ё> в нижнем регистре
+        console.group(`Информация по объекту ${this.name}:`)
+        console.log(`имя объекта ${this.name}`)
+        console.log(`его возраст ${this.age}`)
+        console.log(`место работы ${job}`)
+        console.log(`телефон ${phone}`)
+        console.groupEnd()
     }
 }
 
-const lena = Object.create(person)
-lena.name = 'Elena'
+const lena = {
+    name: 'Лена',
+    age: 23,
+}
 
-const str = new String('Новая строка')
+//person.logInfo.bind(lena)();
+//fnLenaInfoLog()
+
+const fnLenaInfoLog = person.logInfo.bind(lena)
+//fnLenaInfoLog('Программист', '+7(922)-204-3348')
+
+const fnLenaInfoLog1 = person.logInfo.bind(lena, 'Программист', '+7(922)-204-3348')
+//fnLenaInfoLog1()
+
+// call()
+//person.logInfo.call(lena, 'Программист', '+7(922)-204-3348')
+
+// apply()
+//person.logInfo.apply(lena, ['Программист', '+7(922)-204-3348'])
+
+/// ------------------------------------------
+/// Контекст и прототипы
+const array = [1, 2, 3, 4, 5]
+
+function multBy(arr, n){
+    return arr.map(function(item){
+        return item * n
+        })
+}
+// console.log(multBy(array, 5))
+
+Array.prototype.multBy1 = function(n){
+    //console.log('multBy1', this)
+    return this.map(function(item){
+        return item * n
+        })
+}
+// console.log(array.multBy1(5))
+// [10, 20, 30, 40, 50].multBy1(5)
