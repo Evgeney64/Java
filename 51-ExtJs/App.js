d1c668396d7cc9836c979771c458963a6249015c
Евгений Роев <roev64@mail.ru>
2021-09-10 16:36:34 +0500

    20210728-01

--- 51-ExtJs/App.js
+++ 51-ExtJs/App.js
@@ -1,9 +1,40 @@
 Ext.onReady(function(){
-Ext.define('Image', {
-    extend: 'Ext.data.Model',
-    fields: [
-        { name:'src', type:'string' },
-        { name:'caption', type:'string' }
-    ]
-});
-});
+    Ext.create('Ext.Panel', {
+            width: 500,
+            height: 360,
+            padding: 10,
+            layout:'border',
+            items: [{
+                    xtype: 'panel',
+                    title: 'Центральная панель',
+                    html: 'Центральная панель',
+                    region: 'center',
+                    margin: '5 5 5 5'
+                },{
+                    xtype: 'panel',
+                    title: 'Верхняя панель',
+                    html: 'Верхняя панель',
+                    region: 'north',
+                    height: 80
+                },{
+                    xtype: 'panel',
+                    title: 'Нижняя панель',
+                    html: 'Нижняя панель',
+                    region: 'south',
+                    height: 80
+                },{
+                    xtype: 'panel',
+                    title: 'Левая панель',
+                    html: 'Левая панель',
+                    region: 'west',
+                    width: 100
+                },{
+                    xtype: 'panel',
+                    title: 'Правая панель',
+                    html: 'Правая панель',
+                    region: 'east',
+                    width: 120
+                }] ,
+            renderTo: Ext.getBody()
+        });
+ });
